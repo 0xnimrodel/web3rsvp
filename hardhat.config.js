76f06c624b5955b5098e14b3edcf499ab646797c
zepgursoy <34766972+zepgursoy@users.noreply.github.com>
2022-08-07 00:13:11 +0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    }
+  }
+};
+
+//0x5931a300436249b5FCa4D09c5e3262844D87d43e
